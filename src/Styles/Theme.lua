57ea2739d0510cd7216f9de35b91efacff9ec188
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-01 21:01:05 -0800

    Theme.lua

--- src/Styles/Theme.lua
+++ src/Styles/Theme.lua
@@ -1,7 +1,9 @@
 local Theme = {
     MainBackground = Color3.fromRGB(15, 15, 15),
-    Accent = Color3.fromHex("ff0080"),
+    SecondaryBackground = Color3.fromRGB(25, 25, 25),
+    Accent = Color3.fromRGB(255, 255, 255),
     Text = Color3.fromRGB(255, 255, 255),
-    CornerRadius = UDim.new(0, 8)
+    PlaceholderText = Color3.fromRGB(120, 120, 120),
+    CornerRadius = UDim.new(0, 9)
 }
 return Theme
