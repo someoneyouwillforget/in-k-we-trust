806767bb44ca8d16daa3e055269140d495ba4bef
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-01 20:46:02 -0800

    Theme.lua

--- src/Styles/Theme.lua
+++ src/Styles/Theme.lua
@@ -1,21 +1,7 @@
 local Theme = {
-    -- Background Colors
-    MainBackground = Color3.fromRGB(20, 20, 20),      
-    SecondaryBackground = Color3.fromRGB(25, 25, 25), 
-    
-    -- Accent & Branding
-    Accent = Color3.fromRGB(0, 125, 255),             
-    
-    -- Text Colors
-    Text = Color3.fromRGB(255, 255, 255),             
-    SecondaryText = Color3.fromRGB(180, 180, 180),    
-    
-    -- UI Styling
-    CornerRadius = UDim.new(0, 7),                    
-    StrokeTransparency = 0.8,                         
-    
-    -- Scrolling
-    ScrollBarColor = Color3.fromRGB(40, 40, 40)
+    MainBackground = Color3.fromRGB(15, 15, 15),
+    Accent = Color3.fromHex("ffffff"),
+    Text = Color3.fromRGB(255, 255, 255),
+    CornerRadius = UDim.new(0, 12)
 }
-
 return Theme
